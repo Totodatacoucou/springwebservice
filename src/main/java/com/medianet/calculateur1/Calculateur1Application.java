7244c8cbc7cdfc3462e6ec8ce911fc4fc34c136b
Your Name <you@example.com>
2024-02-25 16:48:22 +0100

    premier commit

---
+++ src/main/java/com/medianet/calculateur1/Calculateur1Application.java
@@ -0,0 +1,13 @@
+package com.medianet.calculateur1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculateur1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculateur1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/medianet/calculateur1/Calculateur1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medianet.calculateur1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculateur1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
